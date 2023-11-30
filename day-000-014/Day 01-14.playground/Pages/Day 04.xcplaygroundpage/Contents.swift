fd705e5077c47e7b09f365a3b30762e503d1924e
Ife <il693@nyu.edu>
2023-11-29 23:06:12 -0500

    finished day 04

--- day-000-014/Day 01-14.playground/Pages/Day 04.xcplaygroundpage/Contents.swift
+++ day-000-014/Day 01-14.playground/Pages/Day 04.xcplaygroundpage/Contents.swift
@@ -2,6 +2,45 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+// Type annotations
 
-//: [Next](@next)
+let surname = "Lasso" // Infered String
+var score = 0 // Inferred Integer
+var name: String = "Paul"
+var price: Double = 10
+let pi: Double = 3.141
+var albums: [String] = ["Red", "Fearless"]
+var user: [String: String] = ["id": "@twostraws"]
+var books: Set<String> = Set([
+    "The Bluest Eye",
+    "Foundation",
+    "Girl, Woman, Other"
+])
+var soda: [String] = ["Coke", "Pepsi", "Irn-Bru"]
+var teams: [String] = [String]()
+var cities: [String] = []
+var clues = [String]()
+
+enum UIStyle {
+    case light, dark, system
+}
+var style: UIStyle = UIStyle.light
+style = .dark
+
+let username: String
+// lots of complex logic
+username = "@twostraws"
+// lots of more complex logic
+print(username)
+
+// let scores: Int = "Zero" -> not possible! since the type is not valid
+
+// Checkpoint 2
+// Create an array of strings, then write some code that prints the number of items in the array and also the number of unique items in the array.
+var fruits: [String] = ["Banana", "Apple", "Kiwi", "Mango"]
+var names: [String] = ["Joe", "Joe", "Steve", "Bob"]
+print(fruits.count)
+let uniqueNames = Set(names)
+print(names.count)
+print(uniqueNames.count)
+
