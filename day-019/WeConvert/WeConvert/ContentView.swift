f93b055d4efcac1ac1c22b3c0f475644e6c45b30
Ife <il693@nyu.edu>
2023-12-30 15:10:02 -0500

    did the challenge with a conversion of temperatures

---
+++ day-019/WeConvert/WeConvert/ContentView.swift
@@ -0,0 +1,99 @@
+//
+//  ContentView.swift
+//  WeConvert
+//
+//  Created by Ifeoluwa Lawal on 12/30/23.
+//
+
+import SwiftUI
+
+protocol Converter {
+    func convertInput(input: Int, fromUnit: String, toUnit: String) -> Int
+}
+
+struct TemperatureConverter: Converter {
+    // Fahrenheit to celsius
+    func convertInput(input: Int, fromUnit: String, toUnit: String) -> Int {
+        switch fromUnit {
+        case "celsius":
+            switch toUnit {
+            case "fahrenheit":
+                return (input - 32) * 5 / 9
+            case "kelvin":
+                return input + 273.15
+            default: // default is kelvin
+                return 0
+            }
+        case "fahrenheit":
+            switch toUnit {
+            case "celsius":
+                return (input * 9 / 5) + 32
+            case "kelvin":
+                return (input - 32) * 5/9 + 273.15
+            default: // default is kelvin
+                return 0
+            }
+        case "kelvin":
+            switch toUnit {
+            case "celsius":
+                return input - 273.15
+            case "fahrenheit":
+                return (input - 273.15) * 9/5 + 32
+            default: // default is
+                return 0
+            }
+        default:
+            return 0
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var userValue = 0
+    @State private var currentUnit = "celsius"
+    @State private var conversionUnit = "fahrenheit"
+    
+    let temperatureUnits = ["celsius", "fahrenheit", "kelvin"]
+    var possibleTemperatureConversion: [String] {
+        temperatureUnits.filter { $0 != currentUnit }
+    }
+    
+    func convertUserValue(_ converter: Converter) -> Int {
+        converter.convertInput(input: userValue, fromUnit: currentUnit, toUnit: conversionUnit)
+    }
+    let temperatureConverter = TemperatureConverter()
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Picker("Select input unit", selection: $currentUnit) {
+                    ForEach(temperatureUnits, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                Section("Input your temperature") {
+                    TextField("Input value", value: $userValue, format: .number)
+                }
+                
+                Picker("Conversion unit", selection: $conversionUnit) {
+                    ForEach(possibleTemperatureConversion, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                Section("Your temperature in Celsius") {
+                    Text(convertUserValue(temperatureConverter), format: .number)
+                }
+            }.navigationTitle("WeConvert")
+            .navigationBarTitleDisplayMode(.inline)
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ day-019/WeConvert/WeConvert/WeConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeConvertApp.swift
+//  WeConvert
+//
+//  Created by Ifeoluwa Lawal on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct WeConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
