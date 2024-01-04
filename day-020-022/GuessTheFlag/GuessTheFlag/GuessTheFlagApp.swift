6da0217350e92d25dbba62521c8e8a36213e430d
Ife <il693@nyu.edu>
2024-01-03 19:39:22 -0500

    added a bunch of folders and the GuessTheFlag project

---
+++ day-020-022/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ifeoluwa Lawal on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
