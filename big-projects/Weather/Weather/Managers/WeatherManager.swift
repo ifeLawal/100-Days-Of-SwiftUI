6871a22d1e319c870dfc41fb9b4fb158084a9142
Ife <il693@nyu.edu>
2024-07-06 12:45:03 -0400

    adding all the jsons for swift weather

--- big-projects/Weather/Weather/Managers/WeatherManager.swift
+++ big-projects/Weather/Weather/Managers/WeatherManager.swift
@@ -11,6 +11,16 @@ import CoreLocation
 class WeatherManager {
     var baseUrl3 = "https://api.openweathermap.org/data/3.0/onecall"
     var baseUrl2 = "https://api.openweathermap.org/data/2.5/weather"
+    // 5 day forecast
+    // https://openweathermap.org/forecast5#:~:text=Internal%20parameter
+    // https://api.openweathermap.org/data/2.5/forecast?lat=40.7143&lon=-74.006&appid=ea6cf8ed6924aa6c0f4f0d01e0edf0b6
+    // City forecast
+    // https://openweathermap.org/current#:~:text=Longitude%20of%20the%20location
+    // https://api.openweathermap.org/data/2.5/weather?q=New+York+City&appid=ea6cf8ed6924aa6c0f4f0d01e0edf0b6
+    // Hourly
+    // 
+    // https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid={API key}
+    
     var appId = "hidden"
     var useCelsius = "units=metric"
     
